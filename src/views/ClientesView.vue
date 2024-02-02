e6b14b9e55087e5bd32ccac25956e343dcc0d23c
Nissim Albagli <nissimalbagli@gmail.com>
2024-02-02 20:20:25 -0300

    Se agrego una ruta y su respectivo componente

--- src/views/ClientesView.vue
+++ src/views/ClientesView.vue
@@ -1,9 +1,15 @@
 <script setup>
-
+  import { RouterLink } from 'vue-router';
 </script>
 
 <template>
     <div>
-        <h1>Clientes</h1>
+        <div>
+            <RouterLink :to="{name: 'agregar-cliente'}">
+                Agregar Cliente
+            </RouterLink>
+        </div>
+
+        <h1 class="text-4xl font-extrabold text-slate-500">Clientes</h1>
     </div>
 </template>
