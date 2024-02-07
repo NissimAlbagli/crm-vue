3e7eb03e8de851dd7033433becdd04fed0a096f7
Nissim Albagli <nissimalbagli@gmail.com>
2024-02-06 21:54:18 -0300

    Se configuro fomrkit, se agregaron iconos y se comenzo a trabajar en el formulario y su validacion

--- src/views/NuevoClienteView.vue
+++ src/views/NuevoClienteView.vue
@@ -21,11 +21,51 @@
         <Heading>{{  titulo  }}</Heading>
 
         <div class="mx-auto mt-10 bg-white shadow">
+            <div class="mx-auto md:w-2/3 py-20 px-6">
             <FormKit
                 type="form"
             >
 
-            </FormKit>
+                <FormKit 
+                    type="text"
+                    label="Nombre"
+                    placeholder="Nombre del Cliente"
+                    validation="required"
+                    :validation-messages="{required: 'El Nombre del Cliente es Obligatorio'}"
+                />
+
+                <FormKit 
+                    type="text"
+                    label="Apellido"
+                    placeholder="Apellido del Cliente"
+                    validation="required"
+                    :validation-messages="{required: 'El Apellido del Cliente es Obligatorio'}"
+                />
+
+                <FormKit 
+                    type="text"
+                    label="Email"
+                    placeholder="Email del Cliente"
+                    validation="required|email"
+                    :validation-messages="{required: 'El Email del Cliente es Obligatorio', email: 'Coloca un Email valido'}"
+                />
+
+                <FormKit 
+                    type="text"
+                    label="Teléfono"
+                    placeholder="Teléfono: XXX-XXX-XXXX"
+                    validation="?matches:/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/"
+                    :validation-messages="{ matches: 'El Formato no es valido'}"
+                />
+
+                </FormKit>
+            </div>
         </div>
     </div>
 </template>
+
+<style>
+    .formkit-wrapper {
+        max-width: 100%;
+    }
+</style>
