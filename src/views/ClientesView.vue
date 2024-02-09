3e3c0cc015e5919e96c0aae5e5e257269dce397a
Nissim Albagli <nissimalbagli@gmail.com>
2024-02-09 02:24:11 -0300

    Se separo la logica de los request en una api y un servicio para hacer el codigo mas reutilizable

--- src/views/ClientesView.vue
+++ src/views/ClientesView.vue
@@ -1,6 +1,6 @@
 <script setup>
     import {onMounted, ref, computed} from 'vue';
-    import axios from 'axios';
+    import ClienteService from '../services/ClienteService';
     import RouterLink from '../components/UI/RouterLink.vue';
     import Heading from '../components/UI/Heading.vue';
     import Cliente from '../components/UI/Cliente.vue';
@@ -8,7 +8,7 @@
     const clientes = ref([]);
 
     onMounted(()=> {
-        axios('http://localhost:4000/clientes')
+        ClienteService.obtenerClientes()
             .then(({data}) => clientes.value = data)
             .catch(error => console.log('Hubo un error'))
         })
--- src/views/NuevoClienteView.vue
+++ src/views/NuevoClienteView.vue
@@ -1,5 +1,5 @@
 <script setup>
-    import axios from 'axios';
+    import ClienteService from '../services/ClienteService';
     import { FormKit} from '@formkit/vue';
     import {useRouter} from 'vue-router';
     import RouterLink from '../components/UI/RouterLink.vue';
@@ -14,7 +14,8 @@
     })
 
     const handleSubmit = (data) => {
-        axios.post('http://localhost:4000/clientes', data)
+        data.estado = 1;
+        ClienteService.agregarCliente(data)
             .then(respuesta => {
                 console.log(respuesta);
                 // Redireccionar
